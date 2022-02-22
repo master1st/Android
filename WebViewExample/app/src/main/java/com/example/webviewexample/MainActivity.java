821728c98525c88b706c6eb59c13564d07b329c7
seokhyun <gah1234@naver.com>
2022-02-22 13:08:43 +0900

    WebViewExample

--- WebViewExample/app/src/main/java/com/example/webviewexample/MainActivity.java
+++ WebViewExample/app/src/main/java/com/example/webviewexample/MainActivity.java
@@ -3,12 +3,44 @@ package com.example.webviewexample;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import java.security.Key;
 
 public class MainActivity extends AppCompatActivity {
 
+    private WebView webview;
+    private String url ="https://github.com/master1st";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        webview = (WebView) findViewById(R.id.webview);
+        webview.getSettings().setJavaScriptEnabled(true);
+        webview.loadUrl(url);
+        webview.setWebChromeClient(new WebChromeClient());
+        webview.setWebViewClient(new WebViewClientClass());
+
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if((keyCode == KeyEvent.KEYCODE_BACK) && webview.canGoBack()){
+        webview.goBack();
+        return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+    private class WebViewClientClass extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
     }
 }
