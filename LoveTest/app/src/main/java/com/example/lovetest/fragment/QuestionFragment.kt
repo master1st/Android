a71789fc4e73f9ba0db59a7e9cc043754726a557
seokhyun <gah1234@naver.com>
2022-03-10 13:17:26 +0900

    LOVE TEST exam

--- LoveTest/app/src/main/java/com/example/lovetest/fragment/QuestionFragment.kt
+++ LoveTest/app/src/main/java/com/example/lovetest/fragment/QuestionFragment.kt
@@ -5,7 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import com.example.lovetest.R
+import kotlinx.android.synthetic.main.fragment_question.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -17,8 +20,9 @@ private const val ARG_PARAM2 = "param2"
  * Use the [QuestionFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class QuestionFragment : Fragment() {
+class QuestionFragment : Fragment(),View.OnClickListener {
     // TODO: Rename and change types of parameters
+    lateinit var navController: NavController
     private var param1: String? = null
     private var param2: String? = null
 
@@ -36,6 +40,21 @@ class QuestionFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_question, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        navController = Navigation.findNavController(view)
+        btn_next2.setOnClickListener(this)
+
+    }
+
+    override fun onClick(p0: View?) {
+        when(p0?.id){
+            R.id.btn_next2 -> {
+                navController.navigate(R.id.action_questionFragment_to_selectionFragment)
+            }
+        }
     }
 
     companion object {
--- LoveTest/app/src/main/java/com/example/lovetest/fragment/ResultFragment.kt
+++ LoveTest/app/src/main/java/com/example/lovetest/fragment/ResultFragment.kt
@@ -5,7 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import com.example.lovetest.R
+import kotlinx.android.synthetic.main.fragment_result.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -21,7 +24,8 @@ class ResultFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
-
+    lateinit var navController: NavController
+    var option = -1
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
@@ -35,8 +39,32 @@ class ResultFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+        option = arguments?.getInt("index")?:-1
         return inflater.inflate(R.layout.fragment_result, container, false)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        navController = Navigation.findNavController(view)
+        setResult(option)
+        btn_home.setOnClickListener {
+            navController.navigate(R.id.action_resultFragment_to_mainFragment)
+        }
+    }
+
+    fun setResult(option : Int){
+        when(option){
+            1->{ tv_main.text="You are a QUITTER!"
+            tv_sub.text="You Can let the person easily" }
+            2->{tv_main.text="You should focus on yourself"
+                tv_sub.text="You become really clingy to your ex"}
+            3->{tv_main.text="You should take it easy"
+                tv_sub.text="You can do crazy things no matter what it takes"}
+            4->{tv_main.text="You are pretty mature"
+                tv_sub.text="You can easily accept the break-up"}
+        }
+    }
+
 
     companion object {
         /**
